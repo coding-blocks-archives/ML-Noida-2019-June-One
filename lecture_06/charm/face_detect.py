5346ecdf575b46ab552a81595f4b1ea977691e29
Keenwarrior <anujgargcse@gmail.com>
2019-06-15 17:16:21 +0530

    lecture 6

---
+++ lecture_06/charm/face_detect.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+
+name = input("Enter your name : ")
+
+cap = cv2.VideoCapture(0)
+
+classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+count = 50
+
+face_list = []
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    faces = classifier.detectMultiScale(gray)
+
+    areas = []
+
+    for face in faces:
+        x, y, w, h = face
+        area = w*h
+        areas.append((area, face))
+
+    areas = sorted(areas, reverse=True)
+
+    if len(areas) > 0:
+        face = areas[0][1]
+        x, y, w, h = face
+        face_img = gray[y:y+h, x:x+w]
+        face_img = cv2.resize(face_img, (100, 100))
+        cv2.imshow("video", face_img)
+        face_list.append(face_img.flatten())
+        count-=1
+        print("Loaded", 50-count)
+        if count <= 0:
+            break
+    # shape = gray.shape
+    # small = cv2.resize(gray, (200, 200))
+    # big = cv2.resize(small, (shape[1], shape[0]))
+    if cv2.waitKey(1) > 30:
+        break
+
+
+
+
+cap.release()
+cv2.destroyAllWindows()
